b7e98f036933025831e7054fac5529bf2c8e1e6c
Pedro Emilio Perez Franco <pit.upgop.iti@gmail.com>
2018-09-12 21:08:32 -0500

    Actualizacion de scripts en base a cambios del modelo

--- Scripts MySQL/SP Insertar/SP_insertarCancion.sql
+++ Scripts MySQL/SP Insertar/SP_insertarCancion.sql
@@ -2,9 +2,10 @@ CREATE PROCEDURE insertarCancion(
     IN nombre varchar(40),
     IN duracion TIME,
     IN numero INT,
-    IN album INT
+    IN album INT,
+    IN sentimiento INT
 )
     BEGIN 
     INSERT INTO Canciones
-    VALUES (0,nombre,duracion,numero,album);
+    VALUES (0,nombre,duracion,numero,album,sentimiento);
 END
